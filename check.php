b65332127c496d6ab81402f62ed9f8f59520eb69
Posiang0129 <79617579+Posiang0129@users.noreply.github.com>
2021-03-14 12:30:51 +0800

    Add files via upload

---
+++ check.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo $_POST["degree"] . "<br>"
+
+
+    // if($_POST["e-mail"]=="root" && $_POST["password"]=="123")
+    // {
+    //     header("Location: http://www.nfu.edu.tw");
+    // }
+    // else
+    // {
+    //     header("Location: index.html");
+    // }
+?>
